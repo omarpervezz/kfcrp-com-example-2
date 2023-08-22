3f905f12c31775299582a81cefabd904a2eb5166
Omar Pervez <freelancertusar5182@gmail.com>
2023-08-22 18:26:23 +0600

    done the work

--- assets/resources/js/app.js
+++ assets/resources/js/app.js
@@ -1,3 +1,20 @@
+// Add an event listener to the window that listens for the "scroll" event
+window.addEventListener("scroll", (event) => {
+  // Get the current vertical scroll position using window.scrollY
+  const scroll = window.scrollY;
+
+  // Select the header element using its "header" tag name
+  const header = document.querySelector("header");
+
+  // Check if the scroll position is greater than 20 pixels
+  if (scroll > 20) {
+    // If the condition is true, add the "backDrop & animate__fadeInDownBig" class to the header element
+    header.classList.add("backDrop", "animate__fadeInDownBig");
+  } else {
+    // If the condition is false, remove the "backDrop & animate__fadeInDownBig" class from the header element
+    header.classList.remove("backDrop", "animate__fadeInDownBig");
+  }
+});
 // Get the DOM element with the class "push__bar"
 const push__bar = document.querySelector(".push__bar");
 
@@ -18,45 +35,4 @@ function removeAnimation() {
   // Remove the "animate__backInRight" class to ensure it's not active
   push__bar.classList.remove("animate__backInRight");
 }
-$(document).ready(function () {
-  function initializeSlider() {
-    $("#homepage").fullpage({
-      scrollingSpeed: 700, // Faster scrolling speed
-      autoScrolling: true,
-      fitToSection: true,
-      fitToSectionDelay: 2000,
-      anchors: [
-        "banner",
-        "about-us",
-        "stuff-team",
-        "join-our-server",
-        "community-feedback",
-        "join-us-today",
-        "sponsor-footer",
-      ],
-      verticalCentered: false,
-      navigation: true,
-      responsiveWidth: 768, // Adjust to your desired breakpoint
-      easingcss3: "cubic-bezier(0.645, 0.045, 0.355, 1.000)", // Smooth scrolling
-      onLeave: function (index, nextIndex, direction) {
-        // Your section transition logic
-      },
-    });
-  }
 
-  function destroySlider() {
-    $.fn.fullpage.destroy("all");
-  }
-
-  function handleSliderBasedOnScreenSize() {
-    if (window.innerWidth > 768) {
-      // Adjust the breakpoint as needed
-      initializeSlider();
-    } else {
-      destroySlider();
-    }
-  }
-
-  handleSliderBasedOnScreenSize();
-  $(window).resize(handleSliderBasedOnScreenSize);
-});
